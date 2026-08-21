14b3d7f4a5d18508c7c69670f5b10a738cbbe482
Ahmet Baş <ahba8184@gmail.com>
2026-08-21 17:06:47 +0300

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,13 +4,12 @@ const { getStudentByNumber, getHomeworksByStudentId } = require('../services/use
 
 // Öğrenci Paneli Ana Sayfası
 router.get('/dashboard', async (req, res) => {
-    const studentId = req.query.id; // URL'deki id parametresi (örneğin: ?id=5566)
+    const studentId = req.query.id; // URL'deki id parametresi
 
     try {
         const student = await getStudentByNumber(studentId);
         const homeworks = await getHomeworksByStudentId(studentId);
 
-        // Öğrenci bulunamazsa bile sayfa çökmesin diye varsayılan isim geçiyoruz
         res.render('ogrenci/dashboard', { 
             student: student || { adSoyad: 'Öğrenci' }, 
             homeworks: homeworks || [] 
